bce21d8188d8242c3afe9f1d5c74b12bdfb90916
EarthlySpirituality35 <eric.ou1@education.nsw.gov.au>
2024-08-20 11:09:46 +1000

    20/8/24
    i fixed my dataset, finding a crucial error that prevented my code from running a plot

--- main.py
+++ main.py
@@ -2,19 +2,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-global status; status = 1
+status = 1
 
 my_df = pd.read_csv("Data/spotifydataset.csv", on_bad_lines='warn', encoding='latin-1')
+
 my_df = my_df.drop(columns = ['released_month', 'released_day', 'in_spotify_charts', 'in_spotify_playlists', 'in_shazam_charts', 'in_apple_charts', 'in_apple_playlists', 'in_deezer_charts', 'in_deezer_playlists', 'danceability_%', 'valence_%', 'energy_%', 'acousticness_%', 'instrumentalness_%', 'liveness_%', 'speechiness_%'])
 
-def charts():
+#my_df['streams'] = my_df['streams'].astype(float)
+
+def acharts():
     my_df.plot(
-                kind='bar',
-                x='Country',
-                y='AUD',
+                kind='barh',
+                x='track_name',
+                y='streams',
                 color='blue',
                 alpha=0.3,
-                title='Cost of a Big Mac in AUD')
+                title='Streams of top songs',
+                fontsize=0.1,
+                set_yticks=20(),
+                set_ylabels=20)
     plt.show()
 
 def gui():
@@ -26,9 +32,10 @@ Spotify Dataset
 
 Type:
 1. Print Dataframe
-2. Find specific dataframe
-3. Find the average of a certain category (nearest number)
-4. Quit
+2. A chart of the number of streams of songs
+3. Find specific dataframe
+4. Find the average of a certain category (nearest number)
+5. Quit
         
 
 
@@ -39,7 +46,7 @@ Type:
         if option == 1:
             print(my_df)
         elif option == 2:
-            charts()
+            acharts()
         elif option == 3:
             row = int(input("Enter row number: "))
             print(my_df.iloc[row])
@@ -49,10 +56,10 @@ Type:
         elif option == 5:
             status = 0
         else:
-            print('A number between 1 and 4, come on!')
+            print('Pick another number! ')
 
     except:
-        print('Enter a number, it is not that hard.')
+        print('Enter a number! ')
 
 
 while status == 1:
